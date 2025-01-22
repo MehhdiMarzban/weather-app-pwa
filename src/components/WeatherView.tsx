20089f7ef49281e35419e06d28f4ab3a8a2e6f70
mehdi marzban <marzban7272@gmail.com>
2025-01-22 21:58:23 +0330

    refactor: refactor ShowCurrentWeather

--- src/components/WeatherView.tsx
+++ src/components/WeatherView.tsx
@@ -3,12 +3,26 @@ import { twMerge } from "tailwind-merge";
 interface WeatherViewProps extends React.FC<React.ComponentProps<"div">> {
     Body: React.FC<React.ComponentProps<"div">>;
     Column: React.FC<React.ComponentProps<"div">>;
+    SubItem: React.FC<React.ComponentProps<"div"> & SubItemProps>;
+    HeadItem: React.FC<React.ComponentProps<"div"> & HeadItemProps>;
     Divider: React.FC;
+}
+
+interface HeadItemProps {
+    title: string;
+}
+interface SubItemProps {
+    title: string;
+    value: string;
+    signLeft?: string;
+    signRight?: string;
 }
 
 const WeatherView: WeatherViewProps = ({ children, className, ...rest }) => {
     return (
-        <div className={twMerge("card glass bg-slate-200 bg-opacity-30 shadow-lg w-full", className)} {...rest}>
+        <div
+            className={twMerge("card glass bg-slate-200 bg-opacity-30 shadow-lg w-full", className)}
+            {...rest}>
             {children}
         </div>
     );
@@ -16,9 +30,7 @@ const WeatherView: WeatherViewProps = ({ children, className, ...rest }) => {
 
 const Body: React.FC<React.ComponentProps<"div">> = ({ children, className, ...rest }) => {
     return (
-        <div
-            className={twMerge("card-body grid md:grid-cols-12 grid-cols-1", className)}
-            {...rest}>
+        <div className={twMerge("card-body grid md:grid-cols-12 grid-cols-1", className)} {...rest}>
             {children}
         </div>
     );
@@ -26,8 +38,61 @@ const Body: React.FC<React.ComponentProps<"div">> = ({ children, className, ...r
 
 const Column: React.FC<React.ComponentProps<"div">> = ({ children, className, ...rest }) => {
     return (
-        <div className={twMerge("flex flex-col items-start justify-start col-span-1 space-y-5", className)} {...rest}>
+        <div
+            className={twMerge(
+                "flex flex-col items-start justify-start col-span-1 space-y-5",
+                className
+            )}
+            {...rest}>
             {children}
+        </div>
+    );
+};
+
+const HeadItem: React.FC<React.ComponentProps<"div"> & HeadItemProps> = ({
+    children,
+    className,
+    title,
+    ...rest
+}) => {
+    return (
+        <div
+            className={twMerge("flex flex-row w-full items-center justify-between", className)}
+            {...rest}>
+            <div className="stat-title">{title}</div>
+            {children}
+        </div>
+    );
+};
+
+const SubItem: React.FC<React.ComponentProps<"div"> & SubItemProps> = ({
+    title,
+    value,
+    signRight = null,
+    signLeft = null,
+    className,
+    ...rest
+}) => {
+    return (
+        <div
+            className={twMerge(
+                "stat-value text-primary flex flex-row w-full items-center justify-between",
+                className
+            )}
+            {...rest}>
+            <div className="stat-desc">{title}</div>
+            {(signRight || !signLeft) && (
+                <span>
+                    <span className="text-sm align-middle"> {signRight} </span>
+                    <span dir="rtl">{value}</span>
+                </span>
+            )}
+            {signLeft && (
+                <span>
+                    <span dir="ltr">{value}</span>
+                    <span className="text-sm align-middle mr-1">{signLeft}</span>
+                </span>
+            )}
         </div>
     );
 };
@@ -40,6 +105,8 @@ const Divider: React.FC = () => {
 
 WeatherView.Body = Body;
 WeatherView.Column = Column;
+WeatherView.HeadItem = HeadItem;
+WeatherView.SubItem = SubItem;
 WeatherView.Divider = Divider;
 
 export default WeatherView;
