9e7af7486452b2fcdf9c06728205e61607fe2245
mehdi marzban <marzban7272@gmail.com>
2025-01-29 12:19:29 +0330

    test: change fireEvent to userEvent

--- src/tests/CityList.test.tsx
+++ src/tests/CityList.test.tsx
@@ -1,4 +1,5 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import CityList from "@/components/CityList";
 import { City } from "@/context/AppContext";
 
@@ -7,26 +8,35 @@ describe("CityList", () => {
 
     it("should render nothing if given an empty array", () => {
         const cities: City[] = [];
-        const {container} = render(<CityList cities={cities} handleClickOnCityList={mockHandleClickOnCityList} />);
+        const { container } = render(
+            <CityList cities={cities} handleClickOnCityList={mockHandleClickOnCityList} />
+        );
         const list = screen.queryByRole("list");
         //* this two is like each other
         expect(list).not.toBeInTheDocument();
         expect(container).toBeEmptyDOMElement();
     });
-    it("should render a list of cities and call handleClickOnCityList when a city is clicked", () => {
+    it("should render a list of cities and call handleClickOnCityList when a city is clicked", async () => {
         const cities: City[] = [
             { id: "1", name: "tehran" },
             { id: "2", name: "dorud" },
         ];
+        const user = userEvent.setup();
+
         render(<CityList cities={cities} handleClickOnCityList={mockHandleClickOnCityList} />);
-        cities.forEach((item) => {
+
+        for (const item of cities) {
             const listItemButton = screen.getByText(item.name);
             //* check is in the document
             expect(listItemButton).toBeInTheDocument();
 
-            fireEvent.click(listItemButton);
+            //* fire event is not suitable for interactions and is simple simulate user events
+            // fireEvent.click(listItemButton);
+
+            //* user event is more suitable for interactions and is simulate user events
+            await user.click(listItemButton);
             expect(mockHandleClickOnCityList).toHaveBeenCalledWith(item);
-        });
+        }
         expect(mockHandleClickOnCityList).toHaveBeenCalledTimes(cities.length);
     });
 });
